00670c655833f37abf2e3697492dd1c56774db02
Majdi Aboalila <magdy_sb@hotmail.com>
2026-06-08 17:13:35 +0300

    Automated deployment sync: 08/06/2026 17:13:35

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MWSC7GJZ');
+          `}
+        </Script>
         {/* Google Tag (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-80TSD8MLXB"
@@ -36,6 +46,15 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="min-h-screen bg-background antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MWSC7GJZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Providers>
           {children}
           <CookieBanner />
